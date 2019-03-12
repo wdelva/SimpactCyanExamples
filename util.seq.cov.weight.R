ddfb664e8ee1c96d11833d45fe7410e4609ffc01
David Niyukuri <niyukuri@aims.ac.za>
2019-03-13 01:01:41 +0200

    add fun for renaming IDs in transm net to match tree's labels

--- util.seq.cov.weight.R
+++ util.seq.cov.weight.R
@@ -89,7 +89,7 @@ IDs.Seq.Random.skew <- function(simpact.trans.net = simpact.trans.net,
     trans.sum.men.skew <- dplyr::filter(trans.sum.men, trans.sum.men$age.i>=25 & trans.sum.men$age.i < 40)
     
     trans.sum.women.skew <- dplyr::filter(trans.sum.women, trans.sum.women$age.i>=25 & trans.sum.women$age.i < 40)
-
+    
     # 100 % in the overweighted age group
     perc.100.skew <- nrow(trans.sum.men.skew) + nrow(trans.sum.women.skew) # total number of individuals with age limit
     
@@ -115,3 +115,75 @@ IDs.Seq.Random.skew <- function(simpact.trans.net = simpact.trans.net,
   
   return(samp.all)
 }
+
+
+# Labels of tips -----------------------------
+
+tips.labels <- function(simpact.trans.net = simpact.trans.net,
+                        limitTransmEvents = 7){
+  
+  seeds.id <- length(simpact.trans.net)
+  
+  # Add age at sampling
+  new.transm.tab <- vector("list", seeds.id)
+  
+  for(i in 1:seeds.id){
+    
+    transm.age.i <- as.data.frame(simpact.trans.net[[i]])
+    
+    age.i <- transm.age.i$SampTime - transm.age.i$TOBRec
+    
+    transm.age.i <- cbind(transm.age.i, age.i)
+    
+    new.transm.tab[[i]] <- transm.age.i
+    
+  }
+  
+  # ID numbers of Selected networks with at least limitTransmEvents + 1 indiviuals
+  
+  IDs.transm <- vector()
+  
+  TransmEventsCountVector <- vector()
+  
+  for(k in 1:seeds.id){
+    trans.net.i.check <- as.data.frame(new.transm.tab[[k]])
+    
+    if(nrow(trans.net.i.check)>=limitTransmEvents){
+      
+      TransmEventsCountVector <- c(TransmEventsCountVector, nrow(trans.net.i.check))
+      
+      IDs.transm <- c(IDs.transm, k)
+    }
+  }
+  
+  if(length(IDs.transm)>=1){
+    
+    ## Binding together all selected transmission transmission networks ##
+    
+    for (q in 1:length(IDs.transm)){
+      
+      if(q==1){
+        p <- IDs.transm[q]
+        trans.sum <- new.transm.tab[[p]]
+        rename.id <- paste0(p,".",trans.sum$id,".C")
+        trans.sum$id <- rename.id
+        trans.sum.rename.id <- trans.sum
+      }
+      else{
+        
+        p <- IDs.transm[q]
+        
+        read.trans.sum <- new.transm.tab[[p]]
+        rename.id.read <- paste0(p,".",read.trans.sum$id,".C")
+        read.trans.sum$id <- rename.id.read
+        trans.sum.rename.id <- rbind(trans.sum.rename.id, read.trans.sum)
+      }
+      
+    }
+    
+  }
+    
+    return(trans.sum.rename.id)
+    
+  }
+  
